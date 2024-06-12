27a8a470db3ec0fb8bfdaee7c8e697786197f072
Dmitrii <dima.zabolotnov.02@mail.ru>
2024-06-12 22:27:41 +0300

    Form client validation

--- web/js/form.js
+++ web/js/form.js
@@ -2,9 +2,25 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const FORM_CONTAINER = document.querySelector('.Form');
 
-    let observer = new MutationObserver(mutationRecords=>{
+    let SUBMIT_BTN = FORM_CONTAINER.querySelector('.Form__form__submit');
+    let FORM = FORM_CONTAINER.querySelector('.Form__form');
+
+    FORM.addEventListener('input', reactiveInputValidation);
+    SUBMIT_BTN.addEventListener('click', baseFormValidation);
+
+    //Скрытие показанного уведомления об отправке
+    let observer = new MutationObserver(()=>{
+
+        FORM.removeEventListener('input', reactiveInputValidation);
+        SUBMIT_BTN.removeEventListener('click', baseFormValidation);
 
         const NOTIFICATION = FORM_CONTAINER.querySelector('.Notification');
+
+        FORM = FORM_CONTAINER.querySelector('.Form__form');
+        SUBMIT_BTN = FORM_CONTAINER.querySelector('.Form__form__submit');
+
+        FORM.addEventListener('input', reactiveInputValidation);
+        SUBMIT_BTN.addEventListener('click', baseFormValidation);
 
        if(NOTIFICATION.classList.contains('Notification_show')) {
 
@@ -25,4 +41,66 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 
     observer.observe(FORM_CONTAINER, {childList: true, subtree: true});
+
+
+    function reactiveInputValidation({target}) {
+
+    }
+
+    //Валидация основной формы
+    function baseFormValidation (e) {
+
+        console.log("аафа");
+
+        const formData = new FormData(FORM_CONTAINER.querySelector('.Form__form'));
+
+        let isSuccess = true;
+
+        if(!formData.has('Incoming[agreement]') || formData.get('Incoming[agreement]') !== 'on') isSuccess = false;
+
+        if(!formData.has('Incoming[name]') || !checkNameInput(formData.get('Incoming[name]'))) {
+
+            //отображение ошибки
+            //...
+            isSuccess = false;
+        }
+        if(!formData.has('Incoming[mail]') || !checkMailInput(formData.get('Incoming[mail]'))) {
+
+            //отображение ошибки
+            //...
+            isSuccess = false;
+        }
+        if(!formData.has('Incoming[number]') || !checkNumberInput(formData.get('Incoming[number]'))) {
+
+            //отображение ошибки
+            //...
+            isSuccess = false;
+        }
+        if(!formData.has('Incoming[text]') || !checkTextInput(formData.get('Incoming[text]'))) {
+
+            //отображение ошибки
+            //...
+            isSuccess = false;
+        }
+
+        if (!isSuccess) {
+
+            e.preventDefault();
+
+        } else {
+            FORM_CONTAINER.querySelector('.Notification__blackout').classList.add('Notification__blackout_show');
+        }
+    }
+
+    //Проверка имени
+    function checkNameInput(value) {return (value.length > 2 && value.length <= 70 && value.match(/^(([А-я]+[ ]?)+)$/) !== null);}
+
+    //Проверка почты
+    function checkMailInput(value) {return (value.length !== '' && value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g) !== null);}
+
+    //Проверка телефона
+    function checkNumberInput(value) {return ((value.length === 10 && value.match(/\D/g) === null) || value.length === 0);}
+
+    //Проверка текст-бокса
+    function checkTextInput(value) {return (value.length >= 10 && value.length <= 1200);}
 });
